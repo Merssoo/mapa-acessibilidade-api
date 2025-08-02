75b9a90bdea5371c87d6b1345f9430484050df99
Merssoo <emerson.cardoso.trindade@gmail.com>
2025-08-02 13:47:03 -0300

    Ajuste para deploy em prod

--- src/main/java/com/criativity/mapa_acessibilidade/config/SecurityConfig.java
+++ src/main/java/com/criativity/mapa_acessibilidade/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.criativity.mapa_acessibilidade.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +16,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
+        return http
+                .cors(Customizer.withDefaults()) // 👈 ATIVA o CORS definido no WebConfig
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll())
