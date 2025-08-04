19ec3763db07fffebacbc56eb34adb676f74ff03
Merssoo <emerson.cardoso.trindade@gmail.com>
2025-08-03 22:03:58 -0300

    Ajuste dos testes

--- src/test/java/com/criativity/mapa_acessibilidade/MapaAcessibilidadeApplicationTests.java
+++ src/test/java/com/criativity/mapa_acessibilidade/MapaAcessibilidadeApplicationTests.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop"
+})
 @ActiveProfiles("test")
 class MapaAcessibilidadeApplicationTests {
 
